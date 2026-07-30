60054124e353c040eb2dc9f5bd4950e5c7523778
HIBIKI_5201 <takuto5201@gmail.com>
2026-07-31 02:59:52 +0900

    [update]フレームワークをサブモジュール化

--- Assets/SymphonyFrameWork/Core/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-// Core/Internal/ のヘルパーはフレームワーク専用のため internal のままにするが、
-// アセンブリが分かれているRuntimeとEditorから利用できるようフレンド指定する。
-[assembly: InternalsVisibleTo("SymphonyFrameWork")]
-[assembly: InternalsVisibleTo("SymphonyFrameWork.Editor")]
